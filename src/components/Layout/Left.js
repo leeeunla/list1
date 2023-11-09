7c52b299c372cb5aea59cde2733acccf154f0daa
leeeunla <kazec0123@naver.com>
2023-11-09 09:32:44 +0900

    fill

---
+++ src/components/Layout/Left.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  width: 100%;
+  background-color: lightgoldenrodyellow;
+`;
+
+export function Left() {
+  return (
+    <>
+      <StyledDiv>
+        <h1>Left</h1>
+      </StyledDiv>
+    </>
+  );
+}
